e485ffd7fb129c46de4faf1285c155541cdae50e
blaz <blazyy@gmail.com>
2021-09-01 14:46:40 +0200

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,56 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <Facts v-bind:facts="facts" />
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Facts from "./components/Facts";
+import axios from "axios";
 
 export default {
-  name: 'App',
+  name: "app",
   components: {
-    HelloWorld
-  }
-}
+    Facts,
+  },
+  data() {
+    return {
+      facts: [],
+    };
+  },
+  methods: {
+    created() {
+      axios
+        .get("https://uselessfacts.jsph.pl/random.json?language=en")
+        .then((res) => (this.facts = res.data))
+        .catch((err) => console.log(err));
+    },
+  },
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
 }
+
+body {
+  font-family: Arial, Helvetica, sans-serif;
+  line-height: 1.4;
+}
+
+/*
+.btn {
+  display: inline-block;
+  border: none;
+  background: #555;
+  color: #fff;
+  padding: 7px 20px;
+  cursor: pointer;
+}
+.btn:hover {
+  background: #666;
+}*/
 </style>
---
+++ src/components/Facts.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <div v-bind:key="fact.id" v-for="fact in facts">
+      <h3>{{ fact.title }}</h3>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Facts",
+  props: ["facts"],
+};
+</script>
+
+<style scoped>
+</style>
